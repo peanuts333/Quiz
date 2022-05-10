68cbf232e7a9b5d0e27929dc59fa04d65e0bf3c1
吉田ゆめ <yumejg184@gmail.com>
2022-05-10 19:51:40 +0900

    Initial Commit

---
+++ Quiz/QuizViewController.swift
@@ -0,0 +1,82 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    //クイズを格納する配列
+    var quizArray = [[Any]]()
+    
+    //正解数
+    var correctAnswer: Int = 0
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons1: UIButton!
+    @IBOutlet var choiceButtons2: UIButton!
+    @IBOutlet var choiceButtons3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //------------------------ここから下にクイズを書く------------------------//
+        quizArray.append(["現在(2022年5月時点)活動していないアイドルは？", "ラストアイドル", "℃-ute", "乃木坂46", 2])
+        quizArray.append(["現在のグループ名で正しいのは？", "モーニング娘。", "モーニング娘", "モーニング娘。'22", 3])
+        quizArray.append(["最近5期生が加入したグループは？","乃木坂46","櫻坂46","日向坂46",1])
+        quizArray.append(["2022年3月〜4月に新メンバーの募集が行われたグループは？","乃木坂46","櫻坂46","日向坂46",3])
+        
+       
+        //------------------------ここから上にクイズを書く------------------------//
+        
+        //シャッフルする
+        quizArray.shuffle()
+
+        choiceQuiz()
+    }
+    
+    func choiceQuiz() {
+        quizTextView.text = quizArray[0][0] as? String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        choiceButtons1.setTitle(quizArray[0][1] as? String, for: .normal)
+        choiceButtons2.setTitle(quizArray[0][2] as? String, for: .normal)
+        choiceButtons3.setTitle(quizArray[0][3] as? String, for: .normal)
+    }
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer+=1
+        }
+    
+        
+        quizArray.remove(at: 0)
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
+            performSegueToResult()
+        } else {
+            choiceQuiz()
+        }
+    }
+    
+    func performSegueToResult() {
+        performSegue(withIdentifier: "toResultView", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destination as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
+}
+
+
